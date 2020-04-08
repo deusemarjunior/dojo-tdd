0cb9ff0c7c9fe5e17a3bad38a2615c60f37ef303
Pedro Teles <pedroateles@protonmail.com>
2020-04-08 16:05:17 -0300

    primeiro teste passando

--- password/src/main/kotlin/org/example/PasswordKata.kt
+++ password/src/main/kotlin/org/example/PasswordKata.kt
@@ -2,10 +2,12 @@ package org.example
 
 class PasswordKata (var senha: String){
 
-    fun temMaisDeOito(){
+    fun temMaisDeOito() : Boolean{
         return senha.length>8
     }
 
-
+    fun temLetraMaiuscula() : Boolean {
+        return false
+    }
 
 }
