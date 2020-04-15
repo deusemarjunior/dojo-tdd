4a228361983419fd004fcf53e466ad1f6a37d896
Mathias Sorensen <mathias.sorensen@zup.com.br>
2020-04-15 16:01:14 -0300

    5 full + 1,1,1 start

--- greed-kata/src/main/kotlin/org/example/GreedKata.kt
+++ greed-kata/src/main/kotlin/org/example/GreedKata.kt
@@ -3,8 +3,12 @@ package org.example
 class GreedKata {
 
     fun calculaScore(valor: Int): String {
-        if (valor == 1)
-            return  "100 Points"
+        if (valor == 1) {
+            return "100 Points"
+        } else if( valor == 5 ){
+            return "50 Points"
+        }
+
         return ""
     }
 }
--- greed-kata/src/test/kotlin/org/example/GreedKataTest.kt
+++ greed-kata/src/test/kotlin/org/example/GreedKataTest.kt
@@ -16,7 +16,34 @@ class GreedKataTest {
         var retorno =  greedKata.calculaScore(valor)
 
         //than
-        Assert.assertEquals("100 points", retorno)
+        Assert.assertEquals("100 Points", retorno)
+
+    }
+
+    @Test
+    fun valorCincoRetorna50() {
+
+        //given
+        var valor = 5
+
+        //when
+        var retorno = GreedKata().calculaScore(valor)
+
+        //than
+        Assert.assertEquals("50 Points", retorno);
+
+    }
+
+    @Test
+    fun valorTresUnsRetorna1000(){
+
+       //given
+        var valor = arrayOf(1,1,1)
+
+        //when
+        var retorno = GreedKata().calculaScore(valor)
+
+        //then
 
     }
 
